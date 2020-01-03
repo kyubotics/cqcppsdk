c412f7b0c140d129a2a6b765f26f804d61ac9b76
Richard Chien <richardchienthebest@gmail.com>
2020-01-03 15:28:13 +0800

    Fix unicode reading in dev mode

--- src/dev_mode/main.cpp
+++ src/dev_mode/main.cpp
@@ -1,9 +1,13 @@
-#include "../core/common.h"
-
 #include <signal.h>
 #include <iostream>
 
+#ifdef WIN32
+#include <fcntl.h>
+#include <io.h>
+#endif
+
 #include "../core/api.h"
+#include "../core/common.h"
 #include "../core/event.h"
 #include "../core/init.h"
 #include "../utils/function.h"
@@ -12,6 +16,7 @@
 using namespace std;
 using namespace cq;
 using cq::utils::call_all;
+using cq::utils::ws2s;
 
 static void prompt() { cout << endl << ">>> "; }
 
@@ -41,6 +46,16 @@ int main() {
 
     signal(SIGINT, sig_handler);
 
+#ifdef WIN32
+    _setmode(_fileno(stdin), _O_WTEXT);
+    wstring line;
+    prompt();
+    while (getline(wcin, line)) {
+        if (line.empty()) continue;
+        process(ws2s(line));
+        prompt();
+    }
+#else
     string line;
     prompt();
     while (getline(cin, line)) {
@@ -48,6 +63,7 @@ int main() {
         process(line);
         prompt();
     }
+#endif
 
     call_all(cq::_coolq_exit_callbacks);
     return 0;
