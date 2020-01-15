18355a89a995fc13bd9274691bcdb07756f747de
Richard Chien <richardchienthebest@gmail.com>
2020-01-15 12:23:45 +0800

    Move header file

---
+++ include/cqcppsdk/cqcppsdk.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../../src/core/api.h"
+#include "../../src/core/dir.h"
+#include "../../src/core/event.h"
+#include "../../src/core/event_callback.h"
+#include "../../src/core/exception.h"
+#include "../../src/core/init.h"
+#include "../../src/core/logging.h"
+#include "../../src/core/menu.h"
+#include "../../src/core/message.h"
