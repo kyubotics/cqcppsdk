a50c5ce42cee769e27ef1b383c8807f3cfede80d
Richard Chien <richardchienthebest@gmail.com>
2020-01-18 12:30:16 +0800

    Simplify api.cpp of dev mode

--- src/dev_mode/api.cpp
+++ src/dev_mode/api.cpp
@@ -22,30 +22,29 @@ namespace cq {
 
     static int64_t message_id = 0;
 
+    template <typename... PairFirst, typename... PairSecond>
+    void print_api_call(const string &name, const pair<PairFirst, PairSecond> &... args) {
+        cout << name << endl;
+        ((cout << "  " << args.first << ": " << boolalpha << args.second << endl), ...);
+    }
+
     int64_t send_private_message(const int64_t user_id, const std::string &message) {
-        cout << "send_private_message" << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  message: " << message << endl;
+        print_api_call("send_private_message", pair{"user_id", user_id}, pair{"message", message});
         return ++message_id;
     }
 
     int64_t send_group_message(const int64_t group_id, const std::string &message) {
-        cout << "send_group_message" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  message: " << message << endl;
+        print_api_call("send_group_message", pair{"group_id", group_id}, pair{"message", message});
         return ++message_id;
     }
 
     int64_t send_discuss_message(const int64_t discuss_id, const std::string &message) {
-        cout << "send_discuss_message" << endl;
-        cout << "  discuss_id: " << discuss_id << endl;
-        cout << "  message: " << message << endl;
+        print_api_call("send_discuss_message", pair{"discuss_id", discuss_id}, pair{"message", message});
         return ++message_id;
     }
 
     void delete_message(const int64_t message_id) {
-        cout << "delete_message" << endl;
-        cout << "  message_id: " << message_id << endl;
+        print_api_call("delete_message", pair{"message_id", message_id});
     }
 
 #pragma endregion
@@ -53,9 +52,7 @@ namespace cq {
 #pragma region Friend Operation
 
     void send_like(const int64_t user_id, const int32_t times) {
-        cout << "send_like" << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  times: " << times << endl;
+        print_api_call("send_like", pair{"user_id", user_id}, pair{"times", times});
     }
 
 #pragma endregion
@@ -63,70 +60,55 @@ namespace cq {
 #pragma region Group Operation
 
     void set_group_kick(const int64_t group_id, const int64_t user_id, const bool reject_future_request) {
-        cout << "set_group_kick" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  reject_future_request: " << reject_future_request << endl;
+        print_api_call("set_group_kick",
+                       pair{"group_id", group_id},
+                       pair{"user_id", user_id},
+                       pair{"reject_future_request", reject_future_request});
     }
 
     void set_group_ban(const int64_t group_id, const int64_t user_id, const int64_t duration) {
-        cout << "set_group_ban" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  duration: " << duration << endl;
+        print_api_call(
+            "set_group_ban", pair{"group_id", group_id}, pair{"user_id", user_id}, pair{"duration", duration});
     }
 
     void set_group_anonymous_ban(const int64_t group_id, const std::string &anonymous_flag, const int64_t duration) {
-        cout << "set_group_anonymous_ban" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  anonymous_flag: " << anonymous_flag << endl;
-        cout << "  duration: " << duration << endl;
+        print_api_call("set_group_anonymous_ban",
+                       pair{"group_id", group_id},
+                       pair{"anonymous_flag", anonymous_flag},
+                       pair{"duration", duration});
     }
 
     void set_group_whole_ban(const int64_t group_id, const bool enable) {
-        cout << "set_group_whole_ban" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  enable: " << enable << endl;
+        print_api_call("set_group_whole_ban", pair{"group_id", group_id}, pair{"enable", enable});
     }
 
     void set_group_admin(const int64_t group_id, const int64_t user_id, const bool enable) {
-        cout << "set_group_admin" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  enable: " << enable << endl;
+        print_api_call("set_group_admin", pair{"group_id", group_id}, pair{"user_id", user_id}, pair{"enable", enable});
     }
 
     void set_group_anonymous(const int64_t group_id, const bool enable) {
-        cout << "set_group_anonymous" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  enable: " << enable << endl;
+        print_api_call("set_group_anonymous", pair{"group_id", group_id}, pair{"enable", enable});
     }
 
     void set_group_card(const int64_t group_id, const int64_t user_id, const std::string &card) {
-        cout << "set_group_card" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  card: " << card << endl;
+        print_api_call("set_group_card", pair{"group_id", group_id}, pair{"user_id", user_id}, pair{"card", card});
     }
 
     void set_group_leave(const int64_t group_id, const bool dismiss) {
-        cout << "set_group_leave" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  dismiss: " << dismiss << endl;
+        print_api_call("set_group_leave", pair{"group_id", group_id}, pair{"dismiss", dismiss});
     }
 
     void set_group_special_title(const int64_t group_id, const int64_t user_id, const std::string &special_title,
                                  const int64_t duration) {
-        cout << "set_group_special_title" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  special_title: " << special_title << endl;
-        cout << "  duration: " << duration << endl;
+        print_api_call("set_group_special_title",
+                       pair{"group_id", group_id},
+                       pair{"user_id", user_id},
+                       pair{"special_title", special_title},
+                       pair{"duration", duration});
     }
 
     void set_discuss_leave(const int64_t discuss_id) {
-        cout << "set_discuss_leave" << endl;
-        cout << "  discuss_id: " << discuss_id << endl;
+        print_api_call("set_discuss_leave", pair{"discuss_id", discuss_id});
     }
 
 #pragma endregion
@@ -135,19 +117,16 @@ namespace cq {
 
     void set_friend_request(const std::string &flag, const RequestEvent::Operation operation,
                             const std::string &remark) {
-        cout << "set_friend_request" << endl;
-        cout << "  flag: " << flag << endl;
-        cout << "  operation: " << operation << endl;
-        cout << "  remark: " << remark << endl;
+        print_api_call("set_friend_request", pair{"flag", flag}, pair{"operation", operation}, pair{"remark", remark});
     }
 
     void set_group_request(const std::string &flag, const GroupRequestEvent::SubType &sub_type,
                            const RequestEvent::Operation operation, const std::string &reason) {
-        cout << "set_group_request" << endl;
-        cout << "  flag: " << flag << endl;
-        cout << "  sub_type: " << to_string(sub_type) << endl;
-        cout << "  operation: " << operation << endl;
-        cout << "  reason: " << reason << endl;
+        print_api_call("set_group_request",
+                       pair{"flag", flag},
+                       pair{"sub_type", to_string(sub_type)},
+                       pair{"operation", operation},
+                       pair{"reason", reason});
     }
 
 #pragma endregion
@@ -155,19 +134,17 @@ namespace cq {
 #pragma region QQ Information
 
     int64_t get_login_user_id() {
-        cout << "get_login_user_id" << endl;
+        print_api_call("get_login_user_id");
         return FAKE_LOGIN_USER_ID;
     }
 
     std::string get_login_nickname() {
-        cout << "get_login_nickname" << endl;
+        print_api_call("get_login_nickname");
         return FAKE_LOGIN_NICKNAME;
     }
 
     User get_stranger_info(const int64_t user_id, const bool no_cache) {
-        cout << "get_stranger_info" << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  no_cache: " << no_cache << endl;
+        print_api_call("get_stranger_info", pair{"user_id", user_id}, pair{"no_cache", no_cache});
         User u;
         u.user_id = user_id;
         u.nickname = FAKE_OTHER_NICKNAME;
@@ -177,7 +154,7 @@ namespace cq {
     }
 
     std::vector<Friend> get_friend_list() {
-        cout << "get_friend_list" << endl;
+        print_api_call("get_friend_list");
         Friend f;
         f.user_id = FAKE_OTHER_USER_ID;
         f.nickname = FAKE_OTHER_NICKNAME;
@@ -186,27 +163,23 @@ namespace cq {
     }
 
     std::vector<Group> get_group_list() {
-        cout << "get_group_list" << endl;
+        print_api_call("get_group_list");
         return {};
     }
 
     Group get_group_info(const int64_t group_id, const bool no_cache) {
-        cout << "get_group_info" << endl;
-        cout << "  no_cache: " << no_cache << endl;
+        print_api_call("get_group_info", pair{"group_id", group_id}, pair{"no_cache", no_cache});
         return Group();
     }
 
     std::vector<GroupMember> get_group_member_list(const int64_t group_id) {
-        cout << "get_group_member_list" << endl;
-        cout << "  group_id: " << group_id << endl;
+        print_api_call("get_group_member_list", pair{"group_id", group_id});
         return {};
     }
 
     GroupMember get_group_member_info(const int64_t group_id, const int64_t user_id, const bool no_cache) {
-        cout << "get_group_member_info" << endl;
-        cout << "  group_id: " << group_id << endl;
-        cout << "  user_id: " << user_id << endl;
-        cout << "  no_cache: " << no_cache << endl;
+        print_api_call(
+            "get_group_info", pair{"group_id", group_id}, pair{"user_id", user_id}, pair{"no_cache", no_cache});
         return GroupMember();
     }
 
@@ -215,54 +188,51 @@ namespace cq {
 #pragma region CoolQ
 
     std::string get_cookies(const std::string &domain) {
-        cout << "get_cookies" << endl;
+        print_api_call("get_cookies");
         return "uin=" + to_string(FAKE_LOGIN_USER_ID);
     }
 
     int32_t get_csrf_token() {
-        cout << "get_csrf_token" << endl;
+        print_api_call("get_csrf_token");
         return 123;
     }
 
     std::string get_app_directory() {
-        cout << "get_app_directory" << endl;
+        print_api_call("get_app_directory");
         const auto app_dir = fs::path(get_coolq_root_directory()) / "data" / "app" / "";
         dir::create_dir_if_not_exists(app_dir.string());
         return app_dir.string();
     }
 
     std::string get_coolq_root_directory() {
-        cout << "get_coolq_root_directory" << endl;
+        print_api_call("get_coolq_root_directory");
         string root_dir = FAKE_COOLQ_ROOT_DIR;
         dir::create_dir_if_not_exists(root_dir);
         return fs::absolute(root_dir).string();
     }
 
     std::string get_image(const std::string &file) {
-        cout << "get_image" << endl;
+        print_api_call("get_image");
         return "";
     }
 
     std::string get_record(const std::string &file, const std::string &out_format, const bool full_path) {
-        cout << "get_record" << endl;
+        print_api_call("get_record");
         return "";
     }
 
     bool can_send_image() {
-        cout << "can_send_image" << endl;
+        print_api_call("can_send_image");
         return false;
     }
 
     bool can_send_record() {
-        cout << "can_send_record" << endl;
+        print_api_call("can_send_record");
         return false;
     }
 
     void add_log(const int32_t level, const std::string &tag, const std::string &message) {
-        cout << "add_log" << endl;
-        cout << "  level: " << level << endl;
-        cout << "  tag: " << tag << endl;
-        cout << "  message: " << message << endl;
+        print_api_call("add_log", pair{"level", level}, pair{"tag", tag}, pair{"message", message});
     }
 
 #pragma endregion
