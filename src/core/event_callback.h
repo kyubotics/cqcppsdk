5de94d6d85e057de05b84647f2d3ca058db94171
Richard Chien <richardchienthebest@gmail.com>
2020-01-20 12:38:21 +0800

    Pass std::function by reference

--- src/core/event_callback.h
+++ src/core/event_callback.h
@@ -6,7 +6,7 @@
 namespace cq {
 #define DEF_EVENT(EventName, EventType)                                          \
     extern std::vector<std::function<void(EventType)>> _##EventName##_callbacks; \
-    inline void on_##EventName(std::function<void(EventType)> cb) {              \
+    inline void on_##EventName(const std::function<void(EventType)> &cb) {       \
         _##EventName##_callbacks.push_back(cb);                                  \
     }
 
@@ -14,12 +14,12 @@ namespace cq {
     展开为:
 
     extern std::vector<std::function<void()>> _initialize_callbacks;
-    inline void on_initialize(std::function<void()> cb) {
+    inline void on_initialize(const std::function<void()> &cb) {
         _initialize_callbacks.push_back(cb);
     }
 
     extern std::vector<std::function<void(const PrivateMessageEvent &)>> _private_message_callbacks;
-    inline void on_private_message(std::function<void(const PrivateMessageEvent &)> cb) {
+    inline void on_private_message(const std::function<void(const PrivateMessageEvent &)> &cb) {
         _private_message_callbacks.push_back(cb);
     }
     */
