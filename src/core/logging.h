05dedfcdd6c651d97e19ca1d892dd2aa290cbf9c
Richard Chien <richardchienthebest@gmail.com>
2020-02-06 13:17:08 +0800

    Ignore error in logging module

--- src/core/logging.h
+++ src/core/logging.h
@@ -15,39 +15,43 @@ namespace cq::logging {
         FATAL = 40,
     };
 
-    inline void log(const Level level, const std::string &tag, const std::string &msg) {
-        add_log(static_cast<int32_t>(level), tag, msg);
+    inline void log(const Level level, const std::string &tag, const std::string &msg) noexcept {
+        try {
+            add_log(static_cast<int32_t>(level), tag, msg);
+        } catch (cq::ApiError &) {
+            // 忽略日志错误
+        }
     }
 
-    inline void debug(const std::string &tag, const std::string &msg) {
+    inline void debug(const std::string &tag, const std::string &msg) noexcept {
         log(DEBUG, tag, msg);
     }
 
-    inline void info(const std::string &tag, const std::string &msg) {
+    inline void info(const std::string &tag, const std::string &msg) noexcept {
         log(INFO, tag, msg);
     }
 
-    inline void info_success(const std::string &tag, const std::string &msg) {
+    inline void info_success(const std::string &tag, const std::string &msg) noexcept {
         log(INFOSUCCESS, tag, msg);
     }
 
-    inline void info_recv(const std::string &tag, const std::string &msg) {
+    inline void info_recv(const std::string &tag, const std::string &msg) noexcept {
         log(INFORECV, tag, msg);
     }
 
-    inline void info_send(const std::string &tag, const std::string &msg) {
+    inline void info_send(const std::string &tag, const std::string &msg) noexcept {
         log(INFOSEND, tag, msg);
     }
 
-    inline void warning(const std::string &tag, const std::string &msg) {
+    inline void warning(const std::string &tag, const std::string &msg) noexcept {
         log(WARNING, tag, msg);
     }
 
-    inline void error(const std::string &tag, const std::string &msg) {
+    inline void error(const std::string &tag, const std::string &msg) noexcept {
         log(ERROR, tag, msg);
     }
 
-    inline void fatal(const std::string &tag, const std::string &msg) {
+    inline void fatal(const std::string &tag, const std::string &msg) noexcept {
         log(FATAL, tag, msg);
     }
 } // namespace cq::logging
