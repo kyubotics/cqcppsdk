4e7c17bba4f514702950783e972c1f6e18d8c287
MoeMod <824395314@qq.com>
2020-02-11 19:09:14 +0800

    faster dll func import

--- src/std_mode/api.cpp
+++ src/std_mode/api.cpp
@@ -15,30 +15,21 @@ using cq::utils::string_to_coolq;
 using cq::utils::string_from_coolq;
 
 namespace cq {
-    static vector<function<void(HMODULE)>> api_func_initializers;
-
-    static bool add_func_initializer(const function<void(HMODULE)> &initializer) {
-        api_func_initializers.push_back(initializer);
-        return true;
-    }
-
-    void _init_api() {
-        const auto dll = GetModuleHandleW(L"CQP.dll");
-        for (const auto &initializer : api_func_initializers) {
-            initializer(dll);
-        }
-    }
 
     namespace raw {
-#define FUNC(ReturnType, FuncName, ...)                                                              \
-    typedef __declspec(dllimport) ReturnType(__stdcall *__CQ_##FuncName##_T)(__VA_ARGS__);           \
-    __CQ_##FuncName##_T CQ_##FuncName;                                                               \
-    static bool __dummy_CQ_##FuncName = add_func_initializer([](auto dll) {                          \
-        CQ_##FuncName = reinterpret_cast<__CQ_##FuncName##_T>(GetProcAddress(dll, "CQ_" #FuncName)); \
-    });
+    using cq_bool_t = int32_t;
+#define FUNC(ReturnType, FuncName, ...) static ReturnType(__stdcall *CQ_##FuncName)(__VA_ARGS__) = nullptr;
 #include "./api_funcs.inc"
 #undef FUNC
     } // namespace raw
+
+    void _init_api() {
+        const auto dll = GetModuleHandleW(L"CQP.dll");
+        using namespace raw;
+#define FUNC(ReturnType, FuncName, ...) CQ_##FuncName = reinterpret_cast<decltype(CQ_##FuncName)>(GetProcAddress(dll, "CQ_" #FuncName));
+#include "./api_funcs.inc"
+#undef FUNC
+    }
 
     template <typename T, enable_if_t<is_integral_v<T>> * = nullptr>
     inline decltype(auto) chk(T &&res) noexcept(false) {
