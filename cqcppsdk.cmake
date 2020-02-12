523c6f79e744599fe9b0a0d657e0b25dc92d42e7
Richard Chien <richardchienthebest@gmail.com>
2020-02-12 20:32:21 +0800

    Reformat

--- cqcppsdk.cmake
+++ cqcppsdk.cmake
@@ -1,11 +1,11 @@
-if(MSVC AND NOT (${CMAKE_CXX_FLAGS} MATCHES "/[uU][tT][fF]-8"))
+if (MSVC AND NOT (${CMAKE_CXX_FLAGS} MATCHES "/[uU][tT][fF]-8"))
     message(STATUS "turn on msvc utf-8")
     set(CMAKE_CXX_FLAGS "/utf-8 ${CMAKE_CXX_FLAGS}") # 设置 MSVC 编译器识别 UTF-8 编码的源文件
-endif()
+endif ()
 
-if(WIN32)
+if (WIN32)
     add_definitions(-DWIN32) # 确保 Win32 环境下存在 WIN32 定义
-endif()
+endif ()
 
 # 获取 cqcppsdk 所在路径
 set(_CQCPPSDK_DIR ${CMAKE_CURRENT_LIST_DIR})
@@ -24,32 +24,32 @@ endfunction()
 function(cq_add_app OUT_NAME)
     message(STATUS "dev mode: ${CQCPPSDK_DEV_MODE}")
 
-    if(CQCPPSDK_DEV_MODE)
+    if (CQCPPSDK_DEV_MODE)
         # Dev 模式, 产生可执行文件, 用于脱离酷Q单独测试业务逻辑, 不限平台
         add_definitions(-D_CQ_DEV_MODE)
         file(GLOB_RECURSE _CQCPPSDK_MODE_SOURCE_FILES ${_CQCPPSDK_DIR}/src/dev_mode/*.cpp)
         message(STATUS "add dev mode executable: ${OUT_NAME}")
         add_executable(${OUT_NAME} ${ARGN} ${_CQCPPSDK_SOURCE_FILES} ${_CQCPPSDK_MODE_SOURCE_FILES})
-    else()
+    else ()
         # Std 模式, 产生 Windows 动态链接库, 即可被酷Q加载的插件(进而可打包为 CPK), 仅限 Windows 上使用 MSVC x86 工具链构建
         add_definitions(-D_CQ_STD_MODE)
         file(GLOB_RECURSE _CQCPPSDK_MODE_SOURCE_FILES ${_CQCPPSDK_DIR}/src/std_mode/*.cpp)
         message(STATUS "add std mode dll: ${OUT_NAME}")
         add_library(${OUT_NAME} SHARED ${ARGN} ${_CQCPPSDK_SOURCE_FILES} ${_CQCPPSDK_MODE_SOURCE_FILES})
-    endif()
+    endif ()
 
     if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0)
         # 在 GCC 8.x 环境下使用 std::filesystem 需要链接 stdc++fs
         target_link_libraries(${OUT_NAME} stdc++fs)
-    endif()
+    endif ()
 endfunction()
 
 # 添加安装脚本, 将在指定构建目标构建完成后运行, 可用于拷贝 app.dll 和 app.json 到酷Q应用目录
 function(cq_add_install_script OUT_NAME INSTALL_SCRIPT)
-    if(EXISTS ${INSTALL_SCRIPT})
+    if (EXISTS ${INSTALL_SCRIPT})
         add_custom_command(TARGET ${OUT_NAME}
-                           POST_BUILD
-                           COMMAND
-                           powershell -ExecutionPolicy Bypass -NoProfile -File ${INSTALL_SCRIPT} "$<TARGET_FILE_DIR:${OUT_NAME}>")
-    endif()
+                POST_BUILD
+                COMMAND
+                powershell -ExecutionPolicy Bypass -NoProfile -File ${INSTALL_SCRIPT} "$<TARGET_FILE_DIR:${OUT_NAME}>")
+    endif ()
 endfunction()
