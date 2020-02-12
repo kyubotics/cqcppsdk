b3c5408f9427733a7e6d8eaa7137470b9da6d342
Richard Chien <richardchienthebest@gmail.com>
2020-02-12 20:34:19 +0800

    Add std::decay to ensure correctness

--- include/dolores/traits.hpp
+++ include/dolores/traits.hpp
@@ -6,12 +6,12 @@
 
 namespace dolores {
     template <typename E>
-    using enable_if_derived_from_user_event_t =
-        std::enable_if_t<std::is_base_of_v<cq::UserEvent, E> && !std::is_same_v<cq::UserEvent, E>>;
+    using enable_if_derived_from_user_event_t = std::enable_if_t<
+        std::is_base_of_v<cq::UserEvent, std::decay_t<E>> && !std::is_same_v<cq::UserEvent, std::decay_t<E>>>;
 
     struct Condition;
 
     template <typename T1, typename T2 = Condition>
-    using enable_if_is_condition_t =
-        std::enable_if_t<std::is_base_of_v<Condition, T1> && std::is_base_of_v<Condition, T2>>;
+    using enable_if_is_condition_t = std::enable_if_t<
+        std::is_base_of_v<Condition, std::decay_t<T1>> && std::is_base_of_v<Condition, std::decay_t<T2>>>;
 } // namespace dolores
