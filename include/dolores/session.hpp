2bbd525d1a3edd6ba33f512db0b440e0b057cf63
Richard Chien <richardchienthebest@gmail.com>
2020-02-15 15:29:56 +0800

    add get<T>(key) to Session

--- include/dolores/session.hpp
+++ include/dolores/session.hpp
@@ -3,9 +3,24 @@
 #include <any>
 #include <map>
 #include <string>
+#include <type_traits>
 
 namespace dolores {
     class Session : public std::map<std::string, std::any> {
     public:
+        template <typename T>
+        T get(const key_type &key) {
+            return std::any_cast<T>(this->at(key));
+        }
+
+        template <typename T, typename DefVal, typename = std::enable_if_t<std::is_convertible_v<DefVal, T>>>
+        T get(const key_type &key, DefVal &&default_val) {
+            try {
+                return get<T>(key);
+            } catch (std::out_of_range &) {
+            } catch (std::bad_any_cast &) {
+            }
+            return std::forward<DefVal>(default_val);
+        }
     };
 } // namespace dolores
