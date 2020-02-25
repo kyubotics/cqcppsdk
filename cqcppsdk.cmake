d6d805c905876db2a75f3e884e53dc076ec9023e
moemod <824395314@qq.com>
2020-02-25 14:06:59 +0800

    cmake : add CMakeLists.txt

--- cqcppsdk.cmake
+++ cqcppsdk.cmake
@@ -1,24 +1,10 @@
-if (WIN32)
-    add_definitions(-DWIN32) # 确保 Win32 环境下存在 WIN32 定义
-endif ()
-
-# 检查是否支持构建 std 模式
-if ((MSVC OR MSYS OR MINGW) AND (CMAKE_SIZEOF_VOID_P EQUAL 4))
-    set(CQ_CAN_BUILD_STD_MODE YES)
-endif ()
-
-# 获取 SDK 所在路径
-set(_CQCPPSDK_DIR ${CMAKE_CURRENT_LIST_DIR})
-message(STATUS "cqcppsdk dir: ${_CQCPPSDK_DIR}")
-
-# 递归搜索 core 和 utils 中的源文件
-file(GLOB_RECURSE _CQCPPSDK_SOURCE_FILES ${_CQCPPSDK_DIR}/src/core/*.cpp ${_CQCPPSDK_DIR}/src/utils/*.cpp)
+# 引用 CMakeLists.txt
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR})
 
 # 设置 app id, 必须在 CMakeLists.txt 中调用, 否则无法编译
 macro(cq_set_app_id APP_ID)
     message(STATUS "app id: ${APP_ID}")
     set(_CQ_APP_ID ${APP_ID})
-    add_definitions(-DAPP_ID="${APP_ID}")
 endmacro()
 
 # 添加酷Q应用构建目标, 在调用之前设置 CQCPPSDK_DEV_MODE 为 ON 可构建 dev 模式的可执行文件
@@ -27,10 +13,9 @@ function(cq_add_app OUT_NAME)
 
     if (CQCPPSDK_DEV_MODE)
         # dev 模式, 产生可执行文件, 用于脱离酷Q单独测试业务逻辑, 不限平台
-        add_definitions(-D_CQ_DEV_MODE)
-        file(GLOB_RECURSE _CQCPPSDK_MODE_SOURCE_FILES ${_CQCPPSDK_DIR}/src/dev_mode/*.cpp)
         message(STATUS "add dev mode executable: ${OUT_NAME}")
-        add_executable(${OUT_NAME} ${ARGN} ${_CQCPPSDK_SOURCE_FILES} ${_CQCPPSDK_MODE_SOURCE_FILES})
+        add_executable(${OUT_NAME} ${ARGN})
+        target_link_libraries(${OUT_NAME} cqcppsdk_dev)
     elseif (NOT CQ_CAN_BUILD_STD_MODE)
         # 试图构建 std 模式, 但当前工具链无法构建
         message(FATAL_ERROR "can not build std mode dll with the current toolchain")
@@ -39,26 +24,14 @@ function(cq_add_app OUT_NAME)
         if (NOT OUT_NAME STREQUAL "app")
             message(WARNING "the name of std mode dll is required to be \"app\"")
         endif ()
-        add_definitions(-D_CQ_STD_MODE)
-        file(GLOB_RECURSE _CQCPPSDK_MODE_SOURCE_FILES ${_CQCPPSDK_DIR}/src/std_mode/*.cpp)
+
         message(STATUS "add std mode dll: ${OUT_NAME}")
-        add_library(${OUT_NAME} SHARED ${ARGN} ${_CQCPPSDK_SOURCE_FILES} ${_CQCPPSDK_MODE_SOURCE_FILES})
+        add_library(${OUT_NAME} SHARED ${ARGN})
+        target_link_libraries(${OUT_NAME} cqcppsdk_std)
         set_target_properties(${OUT_NAME} PROPERTIES PREFIX "") # 去除 lib 前缀
     endif ()
 
     target_compile_options(${OUT_NAME} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/utf-8>)
-
-    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        # MinGW 编译时, 静态链接, 修正 stdcall 导出名, 支持导入 stdcall 的 dll (--enable-stdcall-fixup)
-        if (NOT CQCPPSDK_DEV_MODE)
-            target_link_options(${OUT_NAME} PRIVATE -static -Wl,--kill-at,--enable-stdcall-fixup)
-        endif ()
-
-        if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0)
-            # 在 GCC 8.x 环境下使用 std::filesystem 需要链接 stdc++fs
-            target_link_libraries(${OUT_NAME} stdc++fs)
-        endif ()
-    endif ()
 endfunction()
 
 # 添加 std 模式构建目标, 可选参数为源文件列表
